1eb8b4b94a373ce446eedfbd3d976e939e175bdd
HaolinHom <haolinhom@gmail.com>
2020-05-28 17:20:23 +0800

    dev: 011_盛最多水的容器;

---
+++ hot100/011_盛最多水的容器/index.test.js
@@ -0,0 +1,18 @@
+const utils = require('../utils');
+const maxArea = require('./index');
+
+const testData = utils.getEachTestParams([
+  {
+    args: [[1,8,6,2,5,4,8,3,7]],
+    expected: 49,
+  },
+]);
+
+test.each(testData)(
+  '盛最多水的容器(%j): ',
+  (numArr, expected) => {
+    expect(maxArea(numArr)).toEqual(expected);
+  }
+);
+
+// test command: jest hot100/011_盛最多水的容器/index.test.js
