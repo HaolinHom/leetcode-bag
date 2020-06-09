a00d772fe3b8be29ca03a9f1a4972af4968ce6ad
HaolinHom <haolinhom@gmail.com>
2020-06-09 15:14:42 +0800

    feat: 017_电话号码的字母组合;

--- hot100/017_电话号码的字母组合/index.js
+++ hot100/017_电话号码的字母组合/index.js
@@ -4,8 +4,6 @@
  */
 var letterCombinations = function(digits) {
   const cfg = {
-    '0': '',
-    '1': '',
     '2': 'abc',
     '3': 'def',
     '4': 'ghi',
@@ -16,6 +14,39 @@ var letterCombinations = function(digits) {
     '9': 'wxyz',
   };
 
+  const len = digits.length;
+
+  if (len === 0) {
+    return [];
+  }
+
+  let digitArr = [];
+
+  for (let i = 0; i < len; i++) {
+    if (digits[i] === '0' || digits[i] === '1' || digits[i] === '*' || digits[i] === '#') {
+      return [];
+    }
+    digitArr.push(cfg[digits[i]].split(''));
+  }
+
+  if (digitArr.length === 1) {
+    return digitArr[0];
+  }
+
+  function getCombinations(storage, rest) {
+    let result = [];
+    let temp = rest.shift();
+
+    for (let i = 0; i < temp.length; i++) {
+      for (let j = 0; j < storage.length; j++) {
+        result.push(storage[j] + temp[i]);
+      }
+    }
+
+    return rest.length === 0 ? result : getCombinations(result, rest);
+  }
+
+  return getCombinations(digitArr.shift(), digitArr);
 };
 
 module.exports = letterCombinations;
