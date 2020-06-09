887412e7c3bb8377dd36c9d9ffd5565555712cd7
HaolinHom <haolinhom@gmail.com>
2020-06-09 18:05:06 +0800

    dev: 020_有效的括号;

---
+++ hot100/020_有效的括号/index.test.js
@@ -0,0 +1,34 @@
+const utils = require('../../utils');
+const isValid = require('./index');
+
+const testData = utils.getEachTestParams([
+  {
+    args: ["()"],
+    expected: true,
+  },
+  {
+    args: ["()[]{}"],
+    expected: true,
+  },
+  {
+    args: ["(]"],
+    expected: false,
+  },
+  {
+    args: ["([)]"],
+    expected: true,
+  },
+  {
+    args: ["{[]}"],
+    expected: true,
+  },
+]);
+
+test.each(testData)(
+  '* 有效的括号: ',
+  (str, expected) => {
+    expect(isValid(str)).toEqual(expected);
+  }
+);
+
+// test command: jest hot100/020_有效的括号/index.test.js
