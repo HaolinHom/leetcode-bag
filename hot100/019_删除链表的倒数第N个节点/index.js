98b0f155d5a1d603930c86cd1135e1190e5bf4f0
HaolinHom <haolinhom@gmail.com>
2020-06-09 16:18:02 +0800

    feat: 019_删除链表的倒数第N个节点;

--- hot100/019_删除链表的倒数第N个节点/index.js
+++ hot100/019_删除链表的倒数第N个节点/index.js
@@ -11,7 +11,25 @@ function ListNode(val) {
  * @return {ListNode}
  */
 var removeNthFromEnd = function(head, n) {
+  let command = 'head';
 
+  function getAll(head, command) {
+    if (head.next) {
+      command += '.next';
+      return getAll(head.next, command);
+    }
+    return command;
+  }
+
+  command = getAll(head, command);
+
+  for (let i = 1; i < n; i++) {
+    command = command.slice(0, command.lastIndexOf('.next'));
+  }
+
+  eval(`${command} = ${command + '.next'}`);
+
+  return head;
 };
 
 module.exports = removeNthFromEnd;
