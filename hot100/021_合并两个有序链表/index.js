7f05fa33e504bb188bca6dcab99e8338f2bda675
HaolinHom <haolinhom@gmail.com>
2020-06-10 14:51:12 +0800

    refactor: 021_合并两个有序链表;

--- hot100/021_合并两个有序链表/index.js
+++ hot100/021_合并两个有序链表/index.js
@@ -16,16 +16,18 @@ var mergeTwoLists = function(l1, l2) {
 
   let node = result;
 
-  while (l1 || l2) {
-    if ((l1 || { val: Infinity }).val < (l2 || { val: Infinity }).val) {
+  while (l1 && l2) {
+    if (l1.val < l2.val) {
       node.next = l1;
       l1 = l1.next;
     } else {
       node.next = l2;
       l2 = l2.next;
     }
-    node = node.next || node;
+    node = node.next;
   }
+
+  node.next = l1 === null ? l2 : l1;
 
   return result.next;
 };
