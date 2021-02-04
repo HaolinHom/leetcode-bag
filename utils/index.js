7c8c9d43ac8a2946a9f1e893b4fb5494ad310789
HaolinHom <haolinhom@gmail.com>
2021-02-04 16:18:34 +0800

    feat: add utils.createTreeNode method;

--- utils/index.js
+++ utils/index.js
@@ -3,6 +3,12 @@ function ListNode(val) {
   this.next = null;
 }
 
+function TreeNode(val, left, right) {
+  this.val = (val===undefined ? 0 : val);
+  this.left = (left===undefined ? null : left);
+  this.right = (right===undefined ? null : right);
+}
+
 module.exports = {
   namespace: 'utils',
   getEachTestParams(testData) {
@@ -33,5 +39,33 @@ module.exports = {
       });
     }
     return result;
+  },
+  createTreeNode(arr) {
+    let tree = new TreeNode(arr.shift());
+
+    if (arr.length === 0) {
+      return tree;
+    }
+
+    const bfs = function (rootArr) {
+      let storage = [];
+      for (let root of rootArr) {
+        if (arr.length > 0) {
+          root.left = new TreeNode(arr.shift());
+          storage.push(root.left);
+        }
+        if (arr.length > 0) {
+          root.right = new TreeNode(arr.shift());
+          storage.push(root.right);
+        }
+      }
+      if (arr.length > 0) {
+        return bfs(storage);
+      }
+    };
+
+    bfs([tree]);
+
+    return tree;
   },
 };
